272e1716fc9ddada24d69a3ffdc0e38b18455e8e
tensymbols <thetensymbols@gmail.com>
2023-05-19 15:00:11 +0300

    Revert "Delete main.go"
    This reverts commit f433c864

---
+++ cmd/service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"WB_L0/internal/subscriber"
+	"context"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	connString    = "postgres://me:123@localhost:5432/wbdb"
+	stanClusterID = "wbcluster"
+	stanClientID  = "wbclient"
+	channelName   = "orderch"
+
+	serverPort = ":8080"
+)
+
+func main() {
+
+	signal.Ignore(syscall.SIGHUP, syscall.SIGPIPE)
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	pgConn := dbConnect(ctx)
+	defer pgConn.Close(ctx)
+	stanConn := stanConnect()
+	defer stanConn.Close()
+	wbService := startService(ctx, pgConn)
+
+	startServer(wbService)
+
+	sub := subscriber.NewSubscriber(stanConn, wbService, "testsub")
+	sub.Subscribe(channelName)
+<<<<<<< HEAD
+	<-ctx.Done()
+=======
+	<-ctx.Done() // context is canceled after termination signal
+>>>>>>> parent of f433c86 (Delete main.go)
+}
